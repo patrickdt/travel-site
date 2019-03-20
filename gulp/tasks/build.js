a9a3d9b06aaca55b2619909acdfafb7933a93faf
Patrick Tirtapraja <patrick.tpraja@gmail.com>
2019-03-19 23:56:27 -0400

    Created a gulp task that compresses and provides provisioning of CSS and JavaScript files.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,10 +1,38 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 del = require('del'),
-usemin = require('gulp-usemin');
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
+
+gulp.task('previewDist', function() {
+  browserSync.init({
+    notify: false, // to prevent popups on the top right corner
+    // injectChanges: true,
+    server: {
+      baseDir: "dist"
+    }
+  });
+});
 
 gulp.task('deleteDistFolder', function() {
   return del("./dist");
+});
+
+gulp.task('copyGeneralFiles', function() {
+  var pathsToCopy = [
+    './app/**/*',
+    '!./app/index.html',
+    '!./app/assets/images//**',
+    '!./app/assets/styles/**',
+    '!./app/assets/scripts/**',
+    '!./app/temp',
+    '!./app/temp/**'
+  ]
+  return gulp.src(pathsToCopy)
+    .pipe(gulp.dest("./dist"));
 });
 
 gulp.task('optimizeImages', function() {
@@ -19,8 +47,11 @@ gulp.task('optimizeImages', function() {
 
 gulp.task('usemin', function() {
   return gulp.src("./app/index.html")
-    .pipe(usemin())
+    .pipe(usemin({
+      css: [function() {return rev()}, function() {return cssnano()}],
+      js: [function() {return rev()}, function() {return uglify()}]
+    }))
     .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('build', gulp.series('deleteDistFolder', 'optimizeImages', 'usemin'));
+gulp.task('build', gulp.series('deleteDistFolder', 'copyGeneralFiles', 'icons', 'optimizeImages', 'styles', 'scripts', 'usemin'));
